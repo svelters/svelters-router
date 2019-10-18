55082e43ac349bf02ca2c67aab357addacfddaea
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2019-10-18 23:19:16 +0200

    Dynamic route declaration

--- src/components/Router.svelte
+++ src/components/Router.svelte
@@ -1,7 +1,13 @@
 <script>
+  import Route from './Route.svelte'
   import { location } from '../store'
+
+  export let routes = []
 
   location.update(() => window.location)
 </script>
 
+{#each routes as route}
+  <Route path={route.path} component={route.component} />
+{/each}
 <slot></slot>
